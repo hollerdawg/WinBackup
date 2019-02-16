436955eb8cc30b6f34bbb5423dbd6eb9fcecaf4a
Ryan Holleran <holleranr2201@my.uwstout.edu>
2019-02-15 23:22:23 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Create a task using task scheduler to start backup.ps1 with the approprate commands:  
+`backup.ps1 [-DestinationDirectory] <string> [-SourceDirectory] <string[]> [-NoCopy]`
---
+++ backup.ps1
@@ -0,0 +1,71 @@
+Param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$DestinationDirectory,
+
+    [Parameter(Mandatory=$false)]
+    [string]$LogDirectory = "C:\Logs",
+
+    [Parameter(Mandatory=$false)]
+    [int]$NumFilesModified = 1,
+
+    [Parameter(Mandatory=$false)]
+    [int]$ChkInterval = 30,
+
+    [Parameter(Mandatory=$false)]
+    [int]$ThreadsPerSource = 2,
+
+    [Parameter(Mandatory=$false)]
+    [switch]$NoCopy,
+
+    [Parameter(Mandatory=$true, ValueFromRemainingArguments=$true)]
+    [string[]]$SourceDirectory
+)
+
+$TimeStamp = Get-Date -Format "yyyy-MM-dd_hh-mm-ss"
+$DateStamp = Get-Date -Format "yyyy-MM-dd"
+$log_dir_base = [System.IO.Path]::GetFullPath("$(Join-Path -Path "$LogDirectory" "backup_robocopy\$DateStamp")")
+$dest_dir_base = [System.IO.Path]::GetFullPath("$DestinationDirectory")
+$Log = Join-Path -Path "$log_dir_base" "${TimeStamp}_main.log"
+Write-Host "$Log"
+$src_dirs = $SourceDirectory
+$null = New-Item -ItemType "directory" -Path "$log_dir_base" -Force
+
+Add-Content "$Log" "**********************************************"
+Add-Content "$Log" "$TimeStamp"
+
+function mirror_dirs {
+    Param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$Source,
+
+        [Parameter(Mandatory=$true, Position=1)]
+        [string]$Destination,
+
+        [Parameter(Mandatory=$true, Position=2)]
+        [string]$ProcLog
+    )
+    $null = New-Item -ItemType "directory" -Path "$Destination" -Force
+    $command = "Robocopy.exe `"$Source`" `"$Destination`" /MIR /NP `"/LOG:$ProcLog`" /R:0 /DST /TBD /MON:$NumFilesModified /MOT:$ChkInterval /MT:$ThreadsPerSource"
+    Add-Content "$Log" "$command"
+    Add-Content "$ProcLog" "$command"
+    Write-Host "$command"
+    $sb = [scriptblock]::create("$command")
+    if (! $NoCopy) {
+        Start-Job -Name "$Source" -ScriptBlock $sb
+    }
+}
+
+foreach ($src_dir in $src_dirs) {
+    $src_dir = [System.IO.Path]::GetFullPath($src_dir)
+    $src_dir_leaf = Split-Path -Path "$src_dir" -Leaf
+    $dest_dir = Join-Path -Path "$dest_dir_base" "$src_dir_leaf"
+    $log_file = Join-Path -Path "$log_dir_base" "${TimeStamp}_$src_dir_leaf.log"
+    mirror_dirs "$src_dir" "$dest_dir" "$log_file"
+}
+
+get-job | wait-job -Any
+
+Add-Content "$Log" "ERROR! A job exited and wasn't supposed to!"
+Add-Content "$Log" "$(Get-Job | Out-String)"
+
+get-job | stop-job
